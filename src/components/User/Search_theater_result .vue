3215989a1152254917fc978dbf0adfd323bfeb6a
Rohit kumar <rohitkumar17154849@gmail.com>
2023-07-03 13:46:36 +0530

    final commit

--- src/components/User/Search_theater_result .vue
+++ src/components/User/Search_theater_result .vue
@@ -15,16 +15,8 @@
     </div>
     <div class="row">
       <div class="card text-white m-2 style_card " v-for="movie in Movies" v-if="(movie.rating > 0 && movie.rating == selectedRating) || selectedRating === ''">
-        <div class="card-header hb">
-          <h5 class="card-title">{{ movie.name }}</h5>
-          <button type="button" class="style_buttom2 dropdown-toggle dropdown-toggle-split hb" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-              <path
-                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-            </svg>
-          </button>
+        <div class="card-header">
+          <h5 class="card-title text-center">{{ movie.name }}</h5>
         </div>
         <img class="card-img-top style_img" :src="movie.image" alt="Card image cap">
         <div class="card-body">
@@ -98,11 +90,6 @@ color: aliceblue;
 
 .dropdown-toggle::after {
 display: none !important;
-}
-
-.hb {
-display: flex;
-justify-content: space-between;
 }
 
 .style_header {
